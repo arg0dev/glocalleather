903050125eb2f19f8ae94e3125eeeaaf8783db8d
barisalby <baris.albayrak@kns.com.tr>
2023-05-17 22:30:05 +0300

    fix(ARG0-1): Structure has been updated.

--- assets/js/main.js
+++ assets/js/main.js
@@ -744,27 +744,7 @@ function onYouTubePlayerAPIReady() {
                 rel: Object.values(playerProperties)[10],
                 fs: Object.values(playerProperties)[11],
             },
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            },
         }
         );
     });
-
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
-
-    let done = false;
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-            setTimeout(stopVideo, 6000);
-            done = true;
-        }
-    }
-    function stopVideo() {
-        player.stopVideo();
-    }
 }
-
